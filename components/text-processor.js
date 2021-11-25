876148aeb5ffccc70e0bed0951d3f5d7d1622580
Brooke Rahn <brooke.rahn@gmail.com>
2021-11-24 19:54:54 -0800

    create and update components files

---
+++ components/text-processor.js
@@ -0,0 +1,50 @@
+import { Header3 } from "./header3.js";
+import { NavigationBar } from "./navigationbar.js";
+import { IoContainer } from "./io-container.js";
+class TextProcessor {
+  constructor(structure) {
+    // Create project div
+    this.element = document.createElement("div");
+    this.ids = structure.ids;
+    this.classList = structure.classList;
+
+    this.header3 = new Header3(
+      structure.ids.header3,
+      structure.classList.header3,
+      structure.title
+    );
+    this.navigationBar = new NavigationBar(
+      structure.ids.navigationBar,
+      structure.classList.navigationBar,
+      structure.buttons
+    );
+    this.container = new IoContainer(
+      structure.ids.ioContainer,
+      structure.classList.ioContainer
+    );
+  }
+
+  render() {
+    this.element.id = this.ids.textProcessor;
+
+    this.header3.render();
+    this.navigationBar.render();
+    this.container.render();
+
+    this.element.appendChild(this.header3.element);
+    this.element.appendChild(this.navigationBar.element);
+    this.element.appendChild(this.container.element);
+  }
+
+  style() {
+    this.element.classList.add(...this.classList.textProcessor);
+    this.header3.style();
+    this.navigationBar.style();
+    this.container.style();
+  }
+
+  activate() {
+    this.navigationBar.activate();
+  }
+}
+export { TextProcessor };
